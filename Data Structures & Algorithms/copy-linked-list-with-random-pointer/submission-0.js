ec53caed168fe1031270747cff3196ed8eb56702
Constantine Antonakos <dev.con.antonakos@outlook.com>
2026-09-14 11:58:30 -0400

    Add: copy-linked-list-with-random-pointer - submission-0

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-0.js
@@ -0,0 +1,34 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    copyRandomList(head) {
+        let map = new Map()
+        map.set(null, null)
+
+        let cur = head;
+        while(cur){
+            let copy = new Node(cur.val)
+            map.set(cur, copy);
+            cur = cur.next
+        }
+
+        cur = head;
+        while(cur){
+            let copy = map.get(cur)
+            copy.next = map.get(cur.next)
+            copy.random = map.get(cur.random)
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
